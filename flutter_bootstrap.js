13a333725a0d13e217b0e0be1a470a142861e4e7
shou12101990-ctrl <shou12101990-ctrl@users.noreply.github.com>
2026-06-19 03:59:31 +0000

    deploy: 9447805a9cd9d418373a6e8cc72d7c0de8fe82b8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "929618176" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2079887110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
